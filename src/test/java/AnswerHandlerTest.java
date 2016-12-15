934cdb18d4344fc63b1faaf3fa81e20c50e005e5
Stuart Cooper <stucooper86@gmail.com>
2016-12-15 12:39:07 +0000

    Mockito testing AnswerHandler

--- src/test/java/AnswerHandlerTest.java
+++ src/test/java/AnswerHandlerTest.java
@@ -19,22 +19,23 @@
 //     MockitoAnnotations.initMocks(this);
 //   }
 //
-//   @Mock AnswerHandler answermock = spy(new AnswerHandler());
+//   @Mock AnswerHandler answerhandlermock = spy(new AnswerHandler());
 //   @Mock QuestionHash questionmock = new QuestionHash();
 //   @Mock String mockquestion = ("What is the capital of England?");
+//   @Mock String mockanswer = ("London");
 //
-//   @Test
-//   public void testSetQuestion() {
-//     questionmock.getQuestionArray();
-//     when(questionmock.randomQuestion()).thenReturn(mockquestion);
-//     questionmock.randomQuestion();
-//     answermock.setQuestion();
-//     String result = "<speak>What is the capital of England?<break time='50ms'/> 10<break time='50ms'/> 9<break time='50ms'/> 8<break time='50ms'/> 7<break time='50ms'/> 6<break time='50ms'/> 5<break time='50ms'/> 4<break time='50ms'/> 3<break time='50ms'/> 2<break time='50ms'/> 1<break time='50ms'/> TIME'S UP!</speak>";
-//     assertEquals("<speak>What is the capital of England?<break time='50ms'/> 10<break time='50ms'/> 9<break time='50ms'/> 8<break time='50ms'/> 7<break time='50ms'/> 6<break time='50ms'/> 5<break time='50ms'/> 4<break time='50ms'/> 3<break time='50ms'/> 2<break time='50ms'/> 1<break time='50ms'/> TIME'S UP!</speak>", result);
-//   }
-// }
-
-// // ------------------
+// //   @Test
+// //   public void testSetQuestion() {
+// //     questionmock.getQuestionArray();
+// //     when(questionmock.randomQuestion()).thenReturn(mockquestion);
+// //     questionmock.randomQuestion();
+// //     answermock.setQuestion();
+// //     String result = "<speak>What is the capital of England?<break time='50ms'/> 10<break time='50ms'/> 9<break time='50ms'/> 8<break time='50ms'/> 7<break time='50ms'/> 6<break time='50ms'/> 5<break time='50ms'/> 4<break time='50ms'/> 3<break time='50ms'/> 2<break time='50ms'/> 1<break time='50ms'/> TIME'S UP!</speak>";
+// //     assertEquals("<speak>What is the capital of England?<break time='50ms'/> 10<break time='50ms'/> 9<break time='50ms'/> 8<break time='50ms'/> 7<break time='50ms'/> 6<break time='50ms'/> 5<break time='50ms'/> 4<break time='50ms'/> 3<break time='50ms'/> 2<break time='50ms'/> 1<break time='50ms'/> TIME'S UP!</speak>", result);
+// //   }
+// // }
+//
+//
 // // // code for testing
 // // // ------------------
 // // public String checkIfCorrect(final String answer, String teamOneName, String teamTwoName, String teamOneScore, String teamTwoScore, String currentTeamAttribute) {
